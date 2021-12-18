f9d42996a87c1105781f31614629124398550e78
Hichem <hichem.djadoudi@gmail.com>
2021-12-18 15:52:57 +0400

    init

---
+++ Udemy_Training/target/rls/debug/deps/Udemy_Training-0e30c11991da06f3.d
@@ -0,0 +1,5 @@
+c:\Users\djado\Desktop\Rust_Project\Udemy_Training\target\rls\debug\deps\Udemy_Training-0e30c11991da06f3.rmeta: src\main.rs
+
+c:\Users\djado\Desktop\Rust_Project\Udemy_Training\target\rls\debug\deps\Udemy_Training-0e30c11991da06f3.d: src\main.rs
+
+src\main.rs:
---
+++ Udemy_Training/target/rls/debug/deps/Udemy_Training-e05c51342bbbc245.d
@@ -0,0 +1,5 @@
+c:\Users\djado\Desktop\Rust_Project\Udemy_Training\target\rls\debug\deps\Udemy_Training-e05c51342bbbc245.rmeta: src\main.rs
+
+c:\Users\djado\Desktop\Rust_Project\Udemy_Training\target\rls\debug\deps\Udemy_Training-e05c51342bbbc245.d: src\main.rs
+
+src\main.rs:

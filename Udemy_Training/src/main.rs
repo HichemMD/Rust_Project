ad442183c9cb8f3215d3d4436c751f567d1a222a
Hichem <hichem.djadoudi@gmail.com>
2021-12-18 15:59:30 +0400

    test3

--- Udemy_Training/src/main.rs
+++ Udemy_Training/src/main.rs
@@ -2,4 +2,5 @@ fn main() {
     println!("Hello, world! Configuration of Github");
     println!("Trying Modification");
     println!("Trying Modification 2");
+    println!("Trying Modification 3");
 }

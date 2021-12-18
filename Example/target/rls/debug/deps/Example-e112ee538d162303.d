22bd6a612ac8e13f2e1b76730376f769736b6af9
Hichem <hichem.djadoudi@gmail.com>
2021-12-18 15:43:28 +0400

    modif2

---
+++ Example/target/rls/debug/deps/Example-e112ee538d162303.d
@@ -0,0 +1,5 @@
+c:\Users\djado\Desktop\Rust_Project\Example\target\rls\debug\deps\Example-e112ee538d162303.rmeta: src\main.rs
+
+c:\Users\djado\Desktop\Rust_Project\Example\target\rls\debug\deps\Example-e112ee538d162303.d: src\main.rs
+
+src\main.rs:

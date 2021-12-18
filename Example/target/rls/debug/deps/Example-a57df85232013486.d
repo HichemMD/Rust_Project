27bbba710a174eae5771639b9039f2ebb800716a
Hichem <hichem.djadoudi@gmail.com>
2021-12-18 15:48:24 +0400

    sup

--- Example/target/rls/debug/deps/Example-a57df85232013486.d
+++
@@ -1,5 +0,0 @@
-c:\Users\djado\Desktop\Rust_Project\Example\target\rls\debug\deps\Example-a57df85232013486.rmeta: src\main.rs
-
-c:\Users\djado\Desktop\Rust_Project\Example\target\rls\debug\deps\Example-a57df85232013486.d: src\main.rs
-
-src\main.rs:
--- Example/target/rls/debug/deps/Example-e112ee538d162303.d
+++
@@ -1,5 +0,0 @@
-c:\Users\djado\Desktop\Rust_Project\Example\target\rls\debug\deps\Example-e112ee538d162303.rmeta: src\main.rs
-
-c:\Users\djado\Desktop\Rust_Project\Example\target\rls\debug\deps\Example-e112ee538d162303.d: src\main.rs
-
-src\main.rs:
